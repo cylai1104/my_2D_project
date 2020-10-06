3d0d35c90076312ef52ca3f99c22b27dcc5fa109
cylai1104 <vermouth4061@gmail.com>
2020-10-07 00:32:17 +0800

    搖桿

---
+++ My 2D Project/Assets/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    
+    [Header("生成間隔時間"), Range(0f, 3f)]
+    public float interval = 2f;
+
+    private GameObject[] points;
+    public GameObject[] sigs;
+
+    private void Awake()
+    {
+        points = GameObject.FindGameObjectsWithTag("生成點");
+        sigs = GameObject.FindGameObjectsWithTag("音符");
+        InvokeRepeating("Spawn", 0, interval);
+
+        
+    }
+
+    private void Spawn()
+    {
+        int rd = Random.Range(0, points.Length);
+        int rs = Random.Range(0, sigs.Length);
+        Instantiate(sigs[rs], points[rd].transform.position, points[rd].transform.rotation);
+    
+    }
+}
