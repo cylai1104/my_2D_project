1b1510bda76b76255b53e8b7b003be101418c872
cylai1104 <vermouth4061@gmail.com>
2020-10-27 16:09:39 +0800

    搖桿OK

--- My 2D Project/Assets/GameManager.cs
+++ My 2D Project/Assets/GameManager.cs
@@ -5,6 +5,7 @@ public class GameManager : MonoBehaviour
     [Header("虛擬搖桿")]
     public Joystick joystick;
     public Transform target;
+    
     private void Update()
     {
         ControlTarget();
@@ -13,13 +14,31 @@ public class GameManager : MonoBehaviour
 
     private void ControlTarget()
     {
-        if (joystick.Horizontal > 0)
-            //target.localPosition += joystick.Horizontal * Time.deltaTime*Vector3.one;
-            target.Translate(joystick.Horizontal * Time.deltaTime , 0, 0);
-            Vector3 targetScale = target.localScale;
-            targetScale.x *= -1;
-            target.localScale = targetScale;
 
+        target.Translate(joystick.Horizontal *5* Time.deltaTime, 0, 0);
+        if (target.localScale.x < 0)
+        { 
+            if (joystick.Horizontal < 0)
+           
+            {//target.localPosition += joystick.Horizontal * Time.deltaTime*Vector3.one;
+                //target.Translate(joystick.Horizontal * Time.deltaTime, 0, 0);
+                Vector3 targetScale = target.localScale;
+                targetScale.x *= -1;
+                target.localScale = targetScale;
+            }
+        }
+
+        if (target.localScale.x > 0)
+        {
+            if (joystick.Horizontal > 0)
+
+            {//target.localPosition += joystick.Horizontal * Time.deltaTime*Vector3.one;
+                //target.Translate(joystick.Horizontal * Time.deltaTime, 0, 0);
+                Vector3 targetScale = target.localScale;
+                targetScale.x *= -1;
+                target.localScale = targetScale;
+            }
+        }
 
 
 
--- My 2D Project/Assets/SpawnManager.cs
+++ My 2D Project/Assets/SpawnManager.cs
@@ -1,34 +1,56 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class SpawnManager : MonoBehaviour
 {
-    
+    [Header("隨機生成物件")]
+    public Transform SpawnObjectRandom;
     [Header("生成間隔時間")]
     public float interval = 0.5f;
-
+    [Header("全部生成點")]
     private GameObject[] points;
-    public GameObject[]notes;
 
-    //
     private int indexRandom;
 
-    public float[] times;
-    public Vector3[] ppoints;
+    [Space(50)]
+    public Transform spawnObjectAssign;
+    [Header("指定生成時間")]
+    public float[] assignTimes;
+    [Header("指定生成位置")]
+    public int[] assignPoints;
+
+    private int indexAssign;
+    
 
     private void Awake()
     {
         points = GameObject.FindGameObjectsWithTag("生成點");
-        notes = GameObject.FindGameObjectsWithTag("音符");
-        InvokeRepeating("Spawn", 0, interval);
-
+        //notes = GameObject.FindGameObjectsWithTag("音符");
+        InvokeRepeating("SpawnRandom", 0, interval);
+        StartCoroutine(AssignSpawnObject());
         
     }
 
-    private void Spawn()
+    private void SpawnRandom()
     {
-        int rd = Random.Range(0, points.Length);
-        int ns = Random.Range(0, notes.Length);
-        Instantiate(notes[ns], points[rd].transform.position, points[rd].transform.rotation);
+        while (indexRandom == indexAssign) indexRandom = Random.Range(0, points.Length);
+        Instantiate(SpawnObjectRandom, points[indexRandom].transform.position, points[indexRandom].transform.rotation);
     
     }
+    private IEnumerator AssignSpawnObject()
+    {
+        for (int i = 0; i < assignPoints.Length; i++)
+        {
+            yield return new WaitForSeconds(assignTimes[indexAssign]);
+            indexAssign++;
+            Instantiate(spawnObjectAssign, points[indexAssign].transform.position, points[indexRandom].transform.rotation);
+
+
+
+        }
+    
+    }
+        
+    
+    
 }
